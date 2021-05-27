71eb402ffb3f082f82869ad1ada73de3454af097
Ricardo <ricardo.br.pi@gmail.com>
2021-05-26 23:38:12 -0300

    feat: add item to list and remove

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,25 @@
-var valueInput = $(".box_input");
-$('.box_btn').click(function(){
+$(".box_add").on("click", function(){
   var valueInput = $(".box_input").val();
-  alert(valueInput);
+  $(".box_input").val('');
+  $(".list").append('<li>' + '<p>' + valueInput + '</p>' + '<button class="remove_item">X</button><button class="edit_item">Edit</button></li>');
+});
+
+$(".box_remove").click(function(){
+  $(document).find("li").remove();
+});
+
+$(document).on("click", ".remove_item", function () {
+  $(this).parent().remove()
+});
+
+$(document).on("click", ".edit_item", function () {
+  $(this).parent().addClass( "editing" );
+  $(".box_input").focus();
+});
+
+$(".edit_all").on("click", function () {
+  var editedValue = $(".box_input").val();
+  $(document).find(".editing p").html( editedValue );
+  $(".editing").removeClass("editing");
+  $(".box_input").val('');
 });
